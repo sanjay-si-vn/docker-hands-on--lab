b6a379e5cf2afa856fcc033fd374a1b407e88357
Sanjay.s <sanjay.si.769697@gmail.com>
2026-06-11 10:52:20 +0530

    docker lab practice

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Task Manager API Running");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "healthy"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
